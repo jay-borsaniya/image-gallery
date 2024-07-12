97b31d0ce00e06139cfdedf52f495427884bcb59
jay borsaniya <jayborsaniya.softrefine@gmail.com>
2024-07-12 15:48:12 +0530

    prod key updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_KEY: '<Production-API-KEY>',
-  API: '<Production-DB-API>',
+  API_KEY: 'AIzaSyCzGj1iTYGmSxrWUxhap1K2EQiuQofUo6I',
+  API: 'https://image-gallary-4f3d1-default-rtdb.asia-southeast1.firebasedatabase.app',
 };
